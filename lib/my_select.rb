924fc2dde6b8f237cab958c7d7a734e1c039b21f
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-16 20:51:01 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  empty_array = []
+  counter = 0
+  while counter <collection.length
+    if yield(collection[counter])
+      empty_array << collection[counter]
+    end
+    counter += 1
+  end
+  empty_array
 end
