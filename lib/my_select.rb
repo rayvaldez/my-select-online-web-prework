b2fd5359deafed9b4691f76dc01255b3a99dd90e
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-16 20:49:07 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  empty_array = []
+  counter = 0
+  while counter <array.length
+    if yield(array[counter])
+      empty_array <<array[counter]
+    end
+    counter += 1
+  end
+  empty_array
 end
